2d88b72f93c07c68508bee3465fe50008461e54a
phosvur <134946307+phosvur@users.noreply.github.com>
2026-03-22 16:52:48 +0700

    Added loot system, exp bar GUI, enemies now drops exp once killed

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -4,14 +4,17 @@ extends CharacterBody2D
 @export var movement_speed = 20.0
 @export var hp = 10
 @export var knockback_recovery = 3.5
+@export var experience = 1
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var loot_base = get_tree().get_first_node_in_group("loot")
 @onready var sprite = $Sprite2D
 @onready var anim = $AnimationPlayer
 @onready var snd_hit = $snd_hit
 
 var death_anim = preload("res://Enemy/explosion.tscn")
+var exp_gem = preload("res://Objects/experience_gem.tscn")
 
 signal remove_from_array(object)
 
@@ -36,6 +39,10 @@ func death():
 	enemy_death.scale = sprite.scale
 	enemy_death.global_position = global_position
 	get_parent().call_deferred("add_child",enemy_death)
+	var new_gem = exp_gem.instantiate()
+	new_gem.global_position = global_position
+	new_gem.experience = experience
+	loot_base.call_deferred("add_child",new_gem)
 	queue_free()
 		
 func _on_hurt_box_hurt(damage, angle, knockback_amount):
--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,10 @@ var movement_speed = 40.0
 var hp = 80
 var last_movement = Vector2.UP
 
+var experience = 0
+var experience_level = 1
+var collected_experience = 0
+
 # Attacks
 var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
 var tornado = preload("res://Player/Attack/tornado.tscn")
@@ -33,8 +37,13 @@ var enemy_close = []
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%walkTimer")
 
+# GUI
+@onready var expBar = get_node("%ExperienceBar")
+@onready var lblLevel = get_node("%lbl_level")
+
 func _ready():
 	attack()
+	set_expbar(experience, calculate_experiencecap())
 
 func _physics_process(_delta):
 	movement()
@@ -133,3 +142,44 @@ func _on_enemy_detection_area_body_exited(body):
 func game_over():
 	print("Game Over!")
 	get_tree().paused = true
+
+
+func _on_grab_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("loot"):
+		area.target = self
+
+
+func _on_collect_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("loot"):
+		var gem_exp = area.collect()
+		calculate_experience(gem_exp)
+		
+func calculate_experience(gem_exp):
+	var exp_required = calculate_experiencecap()
+	collected_experience += gem_exp
+	if experience + collected_experience >= exp_required: # Level up
+		collected_experience -= exp_required - experience
+		experience_level += 1
+		lblLevel.text = str("Level: ", experience_level)
+		experience = 0
+		exp_required = calculate_experiencecap()
+		calculate_experience(0)
+	else:
+		experience += collected_experience
+		collected_experience = 0
+	
+	set_expbar(experience, exp_required)
+	
+func calculate_experiencecap():
+	var exp_cap = experience_level
+	if experience_level < 20:
+		exp_cap = experience_level*5
+	elif experience_level < 40:
+		exp_cap + 95 * (experience_level-19)*8
+	else:
+		exp_cap = 255 + (experience_level - 39)*12
+	return exp_cap
+		 
+func set_expbar(set_value = 1, set_max_value = 100):
+	expBar.value = set_value
+	expBar.max_value = set_max_value
